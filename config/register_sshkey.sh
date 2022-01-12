b6b5fc4ed39b87c79d3a7185463064f37301c3a6
m12watanabe1a <m12watanabe1a@gmail.com>
2022-01-12 17:08:58 +0900

    update

--- config/register_sshkey.sh
+++ config/register_sshkey.sh
@@ -3,17 +3,21 @@
 organization="HarvestX"
 target=$HOME/.ssh/authorized_keys
 
-echo "# $organization members" > $target
-for row in $(gh api /orgs/$organization/members | \
-  jq -r '.[] | {type: .type, username: .login} | select( .type | contains("User")) | { username: .username } | @base64'
-); do
-  _jq() {
-    echo ${row} | base64 --decode | jq -r ${1}
-  }
-  username=$(_jq '.username')
-  echo "Add: $username"
-  wget -qO- https://github.com/${username}.keys >> $target
-done
+if [ -z "$organization"]
+then
 
-unset target username row organization
+  echo "# $organization members" > $target
+  for row in $(gh api /orgs/$organization/members | \
+    jq -r '.[] | {type: .type, username: .login} | select( .type | contains("User")) | { username: .username } | @base64'
+  ); do
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+    }
+    username=$(_jq '.username')
+    echo "Add: $username"
+    wget -qO- https://github.com/${username}.keys >> $target
+  done
+  unset username row
+fi
+unset organization target
 
