9a8be01368ced07955db004d4df95a00467bc92f
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2022-11-28 13:57:03 +0900

    fix chrome install script

--- install/chrome/install.sh
+++ install/chrome/install.sh
@@ -43,7 +43,7 @@ _linux_install() {
 
   case "$OS" in
   Ubuntu)
-    sudo bash -c 'echo "deb [arch=${1}] http://dl.google.com/linux/chrome/deb/ stable main" >>/etc/apt/sources.list.d/google-chrome.list'
+    sudo bash -c 'echo "deb [arch=$(dpkg --print-architecture)] http://dl.google.com/linux/chrome/deb/ stable main" >>/etc/apt/sources.list.d/google-chrome.list'
     sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo apt-get update && sudo apt-get install -y google-chrome-stable
     ;;
