294d0a913d33236fc509244cbd4bb1aa259c7cba
Aoi WATANABE <40206149+m12watanabe1a@users.noreply.github.com>
2024-02-29 13:51:18 +0900

    Fix nvim setings

--- config/bashrc.d/ros2_common.bash
+++ config/bashrc.d/ros2_common.bash
@@ -22,9 +22,6 @@ colcheck() {
 ros2_cli_setup() {
   # Command
   local ros_ws=$1
-  source /usr/share/colcon_cd/function/colcon_cd.sh
-  source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
-  source /usr/share/vcstool-completion/vcs.bash
   export _colcon_cd_root="$ros_ws"
   export RCUTILS_COLORIZED_OUTPUT=1
 }
